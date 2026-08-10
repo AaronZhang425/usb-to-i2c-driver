f8348da55e65446bc3e3fc3a8e9265206409e90a
Aaron Zhang <aaronbxzhang@gmail.com>
2026-08-09 22:28:05 -0400

    Use the i2c probe and remove functions instead of generic probe and remove for platform devices

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/cdev.h>
+#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 #include <linux/slab.h>
 #include <linux/mod_devicetable.h>
@@ -16,33 +17,67 @@ static const struct of_device_id device_ids[] = {
 
 };
 
-static int device_probe(struct platform_device *platform_device_ptr) {
-    dev_info(
-        &platform_device_ptr->dev,
-        "usb_to_i2c: Probe function is falled\n"
-    );
+static int i2c_deivce_probe(
+    struct i2c_client *client,
+    const struct i2c_device_id *id
+) {
+    pr_info("usb_to_i2c: Probe function\n");
 
-    // if (!device_property_present(dev, "compatible")) {
-    //     pr_err("Cannot get compatiable string");
-    //     return -1;
+    // 0x17 is the temporary hard coded address of the i2c device.
+    // Later should be replaced with a read of device tree
+    if (!client->addr = 0x17) {
+        pr_info("usb_to_i2c: i2c device address does not match target");
 
-    // }
+    }
 
     return 0;
+};
 
+
+// Placeholder probe
+// static int device_probe(struct platform_device *platform_device_ptr) {
+//     dev_info(
+//         &platform_device_ptr->dev,
+//         "usb_to_i2c: Probe function is falled\n"
+//     );
+
+//     // if (!device_property_present(dev, "compatible")) {
+//     //     pr_err("Cannot get compatiable string");
+//     //     return -1;
+
+//     // }
+
+//     return 0;
+
+// }
+
+
+static int i2c_device_remove(struct i2c_lient *client) {
+    pr_info("usb_to_i2c: Probe function\n");
+
+    return 0;
 }
 
-static void device_remove(struct platform_device *platform_device_ptr) {
-    dev_info(
-        &platform_device_ptr->dev,
-        "usb_to_i2c: Remove function is falled\n"
-    );
-
+static struct i2c_device_id i2c_ids[] {
+    {"usb_to_i2c_converter", 0},
+    {}
 }
 
-static struct platform_driver driver_info {
-    .probe = device_probe,
-    .remove = device_remove,
+MODULE DEVICE_TABLE(i2c, i2c_ids);
+
+// Placeholder remove
+// static void device_remove(struct platform_device *platform_device_ptr) {
+//     dev_info(
+//         &platform_device_ptr->dev,
+//         "usb_to_i2c: Remove function is falled\n"
+//     );
+
+// }
+
+static struct i2c_driver driver_info {
+    .probe = i2c_device_probe,
+    .remove = i2c_device_remove,
+    .id_table = i2c_ids,
     .driver = {
         .name = "usb_to_i2c_driver",
         .of_match_table = device_ids
@@ -154,7 +189,7 @@ static void __exit module_end_func(void) {
     unregister_chrdev_region(device_number, MINORMASK + 1);
     
     platform_driver_unregister(&driver_info);
-    
+
 }
     
 module_init(module_init_func);
