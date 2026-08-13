5b5d2cd0071148b4fe1e7c17e44602b56eb684c9
Aaron Zhang <aaronbxzhang@gmail.com>
2026-08-12 23:00:16 -0400

    Move contents of hardware_handler.c to main.c and create the devices in the i2c_device_probe to make devices for every new connected device

--- src/main.c
+++ src/main.c
@@ -10,92 +10,81 @@
 #include <linux/property.h>
 
 #include "file_operations_util.h"
-#include "hardware_handler.h"
 
-// static const struct of_device_id device_ids[] = {
-//     {.compatible = "pipico,usb_to_i2c_converter"},
-//     {} /*Empty element signifies end of list*/
-
-// };
-
-// MODULE_DEVICE_TABLE(of, device_ids);
-
-// static struct i2c_device_id i2c_ids[] {
-//     {"usb_to_i2c_converter", 0},
-//     {}
-// }
-
-// MODULE_DEVICE_TABLE(i2c, i2c_ids);
-
-// static int i2c_deivce_probe(
-//     struct i2c_client *client,
-//     const struct i2c_device_id *id
-// ) {
-//     pr_info("usb_to_i2c: Probe function\n");
-
-//     // struct device = client->dev;
-
-//     // 0x17 is the temporary hard coded address of the i2c device.
-//     // Later should be replaced with a read of device tree
-//     if (!client->addr = 0x17) {
-//         pr_info("usb_to_i2c: i2c device address does not match target");
-//         return -1;
-
-//     }
-
-//     return 0;
-// };
-
-// // Placeholder probe
-// // static int device_probe(struct platform_device *platform_device_ptr) {
-// //     dev_info(
-// //         &platform_device_ptr->dev,
-// //         "usb_to_i2c: Probe function is falled\n"
-// //     );
-
-// //     // if (!device_property_present(dev, "compatible")) {
-// //     //     pr_err("Cannot get compatiable string");
-// //     //     return -1;
-
-// //     // }
-
-// //     return 0;
-
-// // }
-
-
-// static int i2c_device_remove(struct i2c_lient *client) {
-//     pr_info("usb_to_i2c: Probe function\n");
-
-//     return 0;
-// }
-
-// // Placeholder remove
-// // static void device_remove(struct platform_device *platform_device_ptr) {
-// //     dev_info(
-// //         &platform_device_ptr->dev,
-// //         "usb_to_i2c: Remove function is falled\n"
-// //     );
-
-// // }
-
-// static struct i2c_driver driver_info {
-//     .probe = i2c_device_probe,
-//     .remove = i2c_device_remove,
-//     .id_table = i2c_ids,
-//     .driver = {
-//         .name = "usb_to_i2c_driver",
-//         .of_match_table = device_ids
-//     }
-// };
-
-
-// Represents the device number. Contains the major and minor numbers
 static dev_t device_number;
 
 static struct cdev cdev_info;
 
 static struct class *device_class;
+
+static const struct of_device_id device_ids[] = {
+    {.compatible = "pipico,usb_to_i2c_converter"},
+    {} /*Empty element signifies end of list*/
+
+};
+
+MODULE_DEVICE_TABLE(of, device_ids);
+
+static struct i2c_device_id i2c_ids[] = {
+    {"usb_to_i2c_converter", 0},
+    {}
+};
+
+MODULE_DEVICE_TABLE(i2c, i2c_ids);
+
+static int i2c_device_probe(
+    struct i2c_client *client
+) {
+    pr_info("usb_to_i2c: Probe function\n");
+
+    // struct device = client->dev;
+    
+    if (client->addr != 0x17) {
+        pr_info("usb_to_i2c: i2c device address does not match target");
+        return -1;
+
+    }
+
+    if (
+        !device_create(
+            device_class,
+            NULL,
+            device_number,
+            NULL,
+            "usb_to_i2c%d",
+            0
+        )
+    ) {
+        pr_err("usb_to_i2c: Could not create \"usb_to_i2c_class0\" device\n");       
+        return ENOMEM;
+        // goto delete_class;
+        
+    }
+    
+    pr_info("usb_to_i2c: Created device under /sys/class/usb_to_i2c_class0\n");    
+
+    return 0;
+
+};
+
+static void i2c_device_remove(struct i2c_client *client) {
+    pr_info("usb_to_i2c: Probe function\n");
+
+    device_destroy(device_class, device_number);
+
+}
+
+static struct i2c_driver driver_info = {
+    .probe = i2c_device_probe,
+    .remove = i2c_device_remove,
+    .id_table = i2c_ids,
+    .driver = {
+        .name = "usb_to_i2c_driver",
+        .of_match_table = device_ids
+    }
+};
+
+// Represents the device number. Contains the major and minor numbers
 
 static int __init module_init_func(void) {
     pr_notice("Initializing the custom usb_to_i2c module\n");
@@ -104,15 +93,15 @@ static int __init module_init_func(void) {
     // Stores the status of operations
     int status;
     
-    #ifdef STATIC_DEVICE_NUMBER
-        device_number = STATIC_DEVICE_NUMBER;
-        status = register_chrdev_region(device_number, MINORMASK + 1, "usb_to_i2c");
-        
-    #else
-    // Dynamically allocate a region of minor device numbers
-        status = alloc_chrdev_region(&device_number, 0, MINORMASK + 1, "usb_to_i2c");
+#ifdef STATIC_DEVICE_NUMBER
+    device_number = STATIC_DEVICE_NUMBER;
+    status = register_chrdev_region(device_number, MINORMASK + 1, "usb_to_i2c");
     
-    #endif
+#else
+    // Dynamically allocate a region of minor device numbers
+    status = alloc_chrdev_region(&device_number, 0, MINORMASK + 1, "usb_to_i2c");
+
+#endif
     
     if (status) {
         pr_err("usb_to_i2c: Could not reserve a region of device numbers\n");
@@ -150,31 +139,30 @@ static int __init module_init_func(void) {
         
     }
     
-    if (
-        !device_create(
-            device_class,
-            NULL,
-            device_number,
-            NULL,
-            "usb_to_i2c%d",
-            0
-        )
-    ) {
-        pr_err("usb_to_i2c: Could not create \"usb_to_i2c_class0\" device\n");       
-        status = ENOMEM;
-        goto delete_class;
+    // if (
+    //     !device_create(
+    //         device_class,
+    //         NULL,
+    //         device_number,
+    //         NULL,
+    //         "usb_to_i2c%d",
+    //         0
+    //     )
+    // ) {
+    //     pr_err("usb_to_i2c: Could not create \"usb_to_i2c_class0\" device\n");       
+    //     status = ENOMEM;
+    //     goto delete_class;
         
-    }
+    // }
     
-    pr_info("usb_to_i2c: Created device under /sys/class/usb_to_i2c_class0\n");
+    // pr_info("usb_to_i2c: Created device under /sys/class/usb_to_i2c_class0\n");
     
-    return i2c_add_driver(get_i2c_driver_info());
-    // return platform_driver_register(get_i2c_driver_info());
+    return i2c_add_driver(&driver_info);
     // return platform_driver_register(&driver_info);
     
-delete_class:
-    class_unregister(device_class);
-    class_destroy(device_class);
+// delete_class:
+//     class_unregister(device_class);
+//     class_destroy(device_class);
     
 delete_cdev:
     cdev_del(&cdev_info);
@@ -188,14 +176,14 @@ free_device_number:
 static void __exit module_end_func(void) {
     pr_notice("Exiting the custom Kernel module\n");
     
+    // device_destroy(device_class, device_number);
+    i2c_del_driver(&driver_info);
     
-    device_destroy(device_class, device_number);
     class_unregister(device_class);
     class_destroy(device_class);
     cdev_del(&cdev_info);
     unregister_chrdev_region(device_number, MINORMASK + 1);
     
-    i2c_del_driver(get_i2c_driver_info());
     // platform_driver_unregister(&driver_info);
 
 }
